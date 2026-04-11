27b2a8815d9312e8d1a0e74f12a839cfcb35f14e
satriowisnu <satriowisnuap@email.com>
2026-04-11 20:05:20 +0700

    feat: add admin and public navigation bars and update layout structure

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/layouts/sidebar";
+import Navbar from "@/components/layouts/navbar/navbarAdmin";
 
 export default function AdminLayout({
   children,
@@ -8,7 +9,12 @@ export default function AdminLayout({
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-1 bg-gray-50 p-6">{children}</main>
+
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+
+        <main className="flex-1 bg-gray-50 p-6">{children}</main>
+      </div>
     </div>
   );
 }
