d9e2b3a25dda3911a1027f33a4607a106b967c66
satriowisnu <satriowisnuap@gmail.com>
2026-04-13 05:57:33 +0700

    feat: integrate NotificationBell component into NavbarAdmin for improved notification handling (dummy data)

--- src/components/layouts/navbar/navbar-admin.tsx
+++ src/components/layouts/navbar/navbar-admin.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import type { NavbarAdminProps } from "@/types/navbar";
-import { Droplets, Bell, User } from "lucide-react";
+import { Droplets, User } from "lucide-react";
+import NotificationBell from "@/components/layouts/navbar/notification-bell";
 
 export default function NavbarAdmin({
   online,
   lastUpdate,
   fluctuate,
-  notifications,
-  setNotifications,
   DeviceStatusBar,
 }: NavbarAdminProps) {
   return (
@@ -19,10 +18,6 @@ export default function NavbarAdmin({
           <div className="w-9 h-9 bg-blue-600 rounded-xl flex items-center justify-center">
             <Droplets size={18} className="text-white" />
           </div>
-          {/* <div>
-            <h1 className="text-sm font-bold text-blue-600">Smart Dispenser</h1>
-            <p className="text-xs text-gray-400">Admin Panel</p>
-          </div> */}
         </div>
 
         {/* RIGHT */}
@@ -35,17 +30,9 @@ export default function NavbarAdmin({
           />
 
           {/* Notification */}
-          <button
-            onClick={() => setNotifications(0)}
-            className="relative w-9 h-9 bg-gray-50 border border-blue-100 rounded-xl flex items-center justify-center text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition"
-          >
-            <Bell size={16} />
-            {notifications > 0 && (
-              <span className="absolute -top-1 -right-1 w-4 h-4 bg-red-500 rounded-full text-white text-[9px] flex items-center justify-center font-bold">
-                {notifications}
-              </span>
-            )}
-          </button>
+          <div className="flex items-center gap-3">
+            <NotificationBell />
+          </div>
 
           {/* 👤 PROFILE */}
           <div className="flex items-center gap-3 cursor-pointer hover:bg-blue-50 px-3 py-2 rounded-xl transition">
