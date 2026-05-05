a8bc8c898af1fa3b6390175976f7e0c47c2c2edb
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-05-05 22:45:43 +0700

    feat: add cookie configuration for session management in authOptions

--- src/lib/auth/authOptions.ts
+++ src/lib/auth/authOptions.ts
@@ -10,6 +10,21 @@ export const authOptions: NextAuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
 
+  cookies: {
+    sessionToken: {
+      name:
+        process.env.NODE_ENV === "production"
+          ? "__Secure-next-auth.session-token"
+          : "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
+
   providers: [
     CredentialsProvider({
       name: "credentials",
@@ -70,7 +85,7 @@ export const authOptions: NextAuthOptions = {
         token.loginTime = now;
       }
 
-     // console.log("TOKEN REMEMBER:", token.rememberMe);
+      // console.log("TOKEN REMEMBER:", token.rememberMe);
       return token;
     },
 
